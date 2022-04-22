76e661b8679dd608c25a3c024308dc4e5a72041d
Abefe-source <oladegaadeola@gmail.com>
2022-04-22 14:44:25 -0700

    second commit

--- main.h
+++ main.h
@@ -36,7 +36,7 @@ void print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
 
-/* print_bases */
+/* print-bases */
 int print_hex(va_list l, flags_t *f);
 int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
@@ -48,29 +48,29 @@ char *convert(unsigned long int num, int base, int lowercase);
 /* _printf */
 int _printf(const char *format, ...);
 
-/* get_print */
+/* get-print */
 int (*get_print(char s))(va_list, flags_t *);
 
-/* get_flag */
+/* get-flag */
 int get_flag(char s, flags_t *f);
 
-/* print_alpha */
+/* print-alphabeth */
 int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
 
-/* write_funcs */
+/* write-function */
 int _putchar(char c);
 int _puts(char *str);
 
-/* print_custom */
+/* print-custom */
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
 
-/* print_address */
+/* print-Address */
 int print_address(va_list l, flags_t *f);
 
-/* print_percent */
+/* print-percent */
 int print_percent(va_list l, flags_t *f);
 
 #endif
